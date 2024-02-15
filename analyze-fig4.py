38c622936d8ac78f24316c53cf1e1b43ba16fb5a
Tatsuya IKUTA <tikuta@tohoku.ac.jp>
2024-02-15 19:03:56 +0900

    Update Fig. 4a

--- analyze-fig4.py
+++ analyze-fig4.py
@@ -1,109 +1,96 @@
 #!/usr/bin/env python3
-from misc import CSV_JPN_CDS_FILENAME, CSV_GLOBAL_CDS_FILENAME, AM_THRESHOLD_PATHOGENIC, AM_THRESHOLD_BENIGN
 import gpcrdb
 import matplotlib
 matplotlib.use('Agg')
-matplotlib.rc('pdf', fonttype=42)
 import matplotlib.pyplot as plt
 plt.rcParams['font.family'] = "Arial"
 import os
 from utils import VariationType
 import numpy as np
 import pandas as pd
+import ensembl
+import json
+from config import AM_THRESHOLD_BENIGN, AM_THRESHOLD_PATHOGENIC
 
 def analyze_jpn_vs_global():
     jpn_vars = {}
     global_vars = {}
-    for receptor in gpcrdb.get_filtered_receptor_list("receptors.json"):
-        entry_name = receptor['entry_name']
-        receptor_class = receptor['receptor_class']
-        dpath = os.path.join(receptor_class, entry_name)
-
-        with open(os.path.join(dpath, CSV_JPN_CDS_FILENAME)) as f:
+    for receptor in gpcrdb.get_filtered_receptor_list():
+        with open(receptor.ensembl_path) as f:
+            display_name = json.load(f)['display_name']
+        with open(receptor.japan_cds_csv_path) as f:
             for l in f.readlines():
-                cols = l.strip('\n').split('\t')
-                if l.startswith('#'):
-                    header = cols
+                try:
+                    anno = ensembl.Annotation.from_csv_line(l)                    
+                except ensembl.BlankLineError:
                     continue
-                var_type = cols[header.index('#Var_Type')]
-                if var_type == VariationType.MISSENSE.name:
-                    gen_num = cols[header.index('Generic_Num')]
-                    if gen_num == 'None':
-                        gen_num = cols[header.index('Seg')]
-                    mut_name = "{}{}$^{{{}}}${}".format(cols[header.index('Ref_AA')], cols[header.index('Res_Num')], gen_num, cols[header.index('Alt_AA')])
-                    key = entry_name + " " + mut_name
-                    freq = float(cols[header.index('AF')])
-                    try:
-                        patho = float(cols[header.index('pathogenicity')])
-                    except:
-                        patho = -1
-                    
-                    family = receptor_class.split(' ')[1]
-                    rsid = cols[header.index('rsID')]
-                    jpn_vars[key] = (freq, patho, family, rsid)
 
-        with open(os.path.join(dpath, CSV_GLOBAL_CDS_FILENAME)) as f:
+                if anno.var_type != VariationType.MISSENSE:
+                    continue
+
+                if not anno.pathogenicity:
+                    continue
+
+                jpn_vars[(display_name, anno.snv.chromosome, anno.snv.position, anno.snv.ref, anno.snv.alt)] = anno
+
+        with open(receptor.global_cds_csv_path) as f:
             for l in f.readlines():
-                cols = l.strip('\n').split('\t')
-                if l.startswith('#'):
-                    header = cols
+                try:
+                    anno = ensembl.Annotation.from_csv_line(l)                    
+                except ensembl.BlankLineError:
                     continue
-                var_type = cols[header.index('#Var_Type')]
-                if var_type == VariationType.MISSENSE.name:
-                    mut_name = "{}{}$^{{{}}}${}".format(cols[header.index('Ref_AA')], cols[header.index('Res_Num')], cols[header.index('Generic_Num')], cols[header.index('Alt_AA')])
-                    key = entry_name + " " + mut_name
-                    freq = float(cols[header.index('AF')])
-                    try:
-                        patho = float(cols[header.index('pathogenicity')])
-                    except:
-                        patho = -1
-                    global_vars[key] = (freq, patho)
 
-    likely_pathogenic_vars = {k for k, v in jpn_vars.items() if v[1] > AM_THRESHOLD_PATHOGENIC}
-    likely_benign_vars = {k for k, v in jpn_vars.items() if v[1] < AM_THRESHOLD_BENIGN}
-    ambigous_vars = set(jpn_vars.keys()) - likely_pathogenic_vars - likely_benign_vars
-    print([var for var in likely_pathogenic_vars if jpn_vars[var][0] > 0.5])
-    print([var for var in ambigous_vars if jpn_vars[var][0] > 0.5])
+                if anno.var_type != VariationType.MISSENSE:
+                    continue
+
+                if not anno.pathogenicity:
+                    continue
+
+                global_vars[(display_name, anno.snv.chromosome, anno.snv.position, anno.snv.ref, anno.snv.alt)] = anno
+
+    all_var_keys = set(jpn_vars.keys()) | set(global_vars.keys())
+    pathos = {k: jpn_vars[k].pathogenicity if k in jpn_vars.keys() else global_vars[k].pathogenicity for k in all_var_keys}
+    
+    likely_pathogenic_keys = {k for k, v in pathos.items() if AM_THRESHOLD_PATHOGENIC < v}
+    ambigous_keys = {k for k, v in pathos.items() if AM_THRESHOLD_BENIGN <= v <= AM_THRESHOLD_PATHOGENIC}
+    likely_benign_keys = {k for k, v in pathos.items() if v < AM_THRESHOLD_BENIGN}
+
+    likely_pathogenic_freqs = [(jpn_vars[k].snv.AF if k in jpn_vars.keys() else 0, global_vars[k].snv.AF if k in global_vars.keys() else 0) for k in likely_pathogenic_keys]
+    ambigous_freqs = [(jpn_vars[k].snv.AF if k in jpn_vars.keys() else 0, global_vars[k].snv.AF if k in global_vars.keys() else 0) for k in ambigous_keys]
+    likely_benign_freqs = [(jpn_vars[k].snv.AF if k in jpn_vars.keys() else 0, global_vars[k].snv.AF if k in global_vars.keys() else 0) for k in likely_benign_keys]
 
     # For Fig. 4a
-    fig, ax = plt.subplots(1, 1, figsize=(5, 4), dpi=300)
+    fig, ax = plt.subplots(1, 1, figsize=(4, 4), dpi=300)
 
+    ax.fill_between([-1, 2], [-0.75, 2.25], [-1.25, 1.75], color='lightgray', lw=0, alpha=0.8, zorder=-1000)
+    ax.fill_between([-1, 2], [-0.5, 2.5], [-1.5, 1.5], color='lightgray', lw=0, alpha=0.6, zorder=-900)
+    ax.fill_between([-1, 2], [-0.25, 2.75], [-1.75, 1.25], color='lightgray', lw=0, alpha=0.4, zorder=-800)
+
+    # AF <= 0.25 will be plotted in another fig
+    ax.fill_between([0, 0.25], [0, 0], [0.25, 0.25], color='white', lw=0, zorder=-600)
+    ax.text(0.125, 0.125, "Fig. S4a", va='center', ha='center', color='lightgray', zorder=-500)
+
+    # Plot only AF > 0.25
+    ax.scatter(*np.array([freqs for freqs in likely_benign_freqs if freqs[0] > 0.25 or freqs[1] > 0.25]).T, marker='.', color='tab:gray', label="likely benign", zorder=1)
+    ax.scatter(*np.array([freqs for freqs in ambigous_freqs if freqs[0] > 0.25 or freqs[1] > 0.25]).T, marker='.', color='tab:orange', label="ambigous", zorder=10)
+    ax.scatter(*np.array([freqs for freqs in likely_pathogenic_freqs if freqs[0] > 0.25 or freqs[1] > 0.25]).T, marker='D', s=10, color='tab:orange', label="likely pathogenic", zorder=100)
+
+    ax.legend()
     lims = (-0.02, 1.02)
-
-    ax.fill_between([-1, 2], [-0.75, 2.25], [-1.25, 1.75], color='#e5e5e5', lw=0)
-    ax.fill_between([-1, 2], [-0.5, 2.5], [-1.5, 1.5], color='#e5e5e5', alpha=0.5, lw=0)
-    ax.fill_between([-1, 2], [-0.25, 2.75], [-1.75, 1.25], color='#e5e5e5', alpha=0.5, lw=0)
-
-    ax.fill_between([0, 0.25], [0, 0], [0.25, 0.25], color='white', lw=0)
-    ax.text(0.125, 0.125, "Fig. S4a", va='center', ha='center', color='lightgray')
-
-    likely_pathogenic = np.array([(jpn_vars.get(k, [0])[0], global_vars.get(k, [0])[0]) for k in likely_pathogenic_vars])
-    likely_benign = np.array([(jpn_vars.get(k, [0])[0], global_vars.get(k, [0])[0]) for k in likely_benign_vars])
-    ambigous = np.array([(jpn_vars.get(k, [0])[0], global_vars.get(k, [0])[0]) for k in ambigous_vars])
-
-    likely_pathogenic_25 = np.array([v for v in likely_pathogenic if v[0] > 0.25 or v[1] > 0.25])
-    likely_benign_25 = np.array([v for v in likely_benign if v[0] > 0.25 or v[1] > 0.25])
-    ambigous_25 = np.array([v for v in ambigous if v[0] > 0.25 or v[1] > 0.25])
-
-    ax.scatter(likely_benign_25.T[0], likely_benign_25.T[1], marker='.', color='tab:gray', label="likely benign")
-    ax.scatter(ambigous_25.T[0], ambigous_25.T[1], marker='.', color='tab:orange', label="ambigous")
-    ax.scatter(likely_pathogenic_25.T[0], likely_pathogenic_25.T[1], marker='D', s=10, color='tab:orange', label="likely pathogenic")
-
-    ax.legend(bbox_to_anchor=(0.5, 0.75), loc='lower right', fontsize=9)
-
+    ticks = [0, 0.25, 0.5, 0.75, 1]
     ax.set_xlim(*lims)
-    ax.set_xticks([0, 0.25, 0.5, 0.75, 1])
-    ax.set_xticklabels([0, 0.25, 0.5, 0.75, 1], size=9)
-    ax.set_yticks([0, 0.25, 0.5, 0.75, 1])
-    ax.set_yticklabels([0, 0.25, 0.5, 0.75, 1], size=9)
+    ax.set_xticks(ticks)
+    ax.set_xticklabels(ticks)
+    ax.set_yticks(ticks)
+    ax.set_yticklabels(ticks)
     ax.set_ylim(*lims)
     ax.set_aspect('equal')
-    ax.set_xlabel("Allele Freq. in 54KJPN", size=9)
-    ax.set_ylabel("Allele Freq. in 1KGP", size=9)
+    ax.set_xlabel("Allele Freq. in 54KJPN")
+    ax.set_ylabel("Allele Freq. in 1KGP")
     fig.tight_layout()
     fig.savefig("./figures/4a_jpn_vs_global.pdf")
     plt.close(fig)
-
+"""
     # For Fig. 4b
     pathogenic_vars = {
         "GPCR": [], 
@@ -212,7 +199,7 @@ def analyze_jpn_vs_global():
     
     df = pd.DataFrame(pathogenic_vars).sort_values('AM pathogenicity', ascending=False)
     df.to_csv("./tables/S1_jpn_pathogenic_vars.csv")
-
+"""
 def analyze_alpha_missense_pathogenicity():
     jpn_vars = {}
     for receptor in gpcrdb.get_filtered_receptor_list("receptors.json"):
@@ -267,4 +254,4 @@ def analyze_alpha_missense_pathogenicity():
 
 if __name__ == '__main__':
     analyze_jpn_vs_global()
-    analyze_alpha_missense_pathogenicity()
+    # analyze_alpha_missense_pathogenicity()
