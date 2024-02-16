ade718b0aec578944d358a50077e04563beb9982
Tatsuya IKUTA <tikuta@tohoku.ac.jp>
2024-02-16 15:48:11 +0900

    Update Fig. S4a

--- analyze-fig4.py
+++ analyze-fig4.py
@@ -51,13 +51,13 @@ def analyze_jpn_vs_global():
     all_var_keys = set(jpn_vars.keys()) | set(global_vars.keys())
     pathos = {k: jpn_vars[k].pathogenicity if k in jpn_vars.keys() else global_vars[k].pathogenicity for k in all_var_keys}
     
-    likely_pathogenic_keys = {k for k, v in pathos.items() if AM_THRESHOLD_PATHOGENIC < v}
-    ambigous_keys = {k for k, v in pathos.items() if AM_THRESHOLD_BENIGN <= v <= AM_THRESHOLD_PATHOGENIC}
-    likely_benign_keys = {k for k, v in pathos.items() if v < AM_THRESHOLD_BENIGN}
+    likely_pathogenic_keys = (k for k, v in pathos.items() if AM_THRESHOLD_PATHOGENIC < v)
+    ambigous_keys = (k for k, v in pathos.items() if AM_THRESHOLD_BENIGN <= v <= AM_THRESHOLD_PATHOGENIC)
+    likely_benign_keys = (k for k, v in pathos.items() if v < AM_THRESHOLD_BENIGN)
 
-    likely_pathogenic_freqs = [(jpn_vars[k].snv.AF if k in jpn_vars.keys() else 0, global_vars[k].snv.AF if k in global_vars.keys() else 0) for k in likely_pathogenic_keys]
-    ambigous_freqs = [(jpn_vars[k].snv.AF if k in jpn_vars.keys() else 0, global_vars[k].snv.AF if k in global_vars.keys() else 0) for k in ambigous_keys]
-    likely_benign_freqs = [(jpn_vars[k].snv.AF if k in jpn_vars.keys() else 0, global_vars[k].snv.AF if k in global_vars.keys() else 0) for k in likely_benign_keys]
+    likely_pathogenic_freqs = {k: (jpn_vars[k].snv.AF if k in jpn_vars.keys() else 0, global_vars[k].snv.AF if k in global_vars.keys() else 0) for k in likely_pathogenic_keys}
+    ambigous_freqs = {k: (jpn_vars[k].snv.AF if k in jpn_vars.keys() else 0, global_vars[k].snv.AF if k in global_vars.keys() else 0) for k in ambigous_keys}
+    likely_benign_freqs = {k: (jpn_vars[k].snv.AF if k in jpn_vars.keys() else 0, global_vars[k].snv.AF if k in global_vars.keys() else 0) for k in likely_benign_keys}
 
     # For Fig. 4a
     fig, ax = plt.subplots(1, 1, figsize=(4, 4), dpi=300)
@@ -71,9 +71,12 @@ def analyze_jpn_vs_global():
     ax.text(0.125, 0.125, "Fig. S4a", va='center', ha='center', color='lightgray', zorder=-500)
 
     # Plot only AF > 0.25
-    ax.scatter(*np.array([freqs for freqs in likely_benign_freqs if freqs[0] > 0.25 or freqs[1] > 0.25]).T, marker='.', color='tab:gray', label="likely benign", zorder=1)
-    ax.scatter(*np.array([freqs for freqs in ambigous_freqs if freqs[0] > 0.25 or freqs[1] > 0.25]).T, marker='.', color='tab:orange', label="ambigous", zorder=10)
-    ax.scatter(*np.array([freqs for freqs in likely_pathogenic_freqs if freqs[0] > 0.25 or freqs[1] > 0.25]).T, marker='D', s=10, color='tab:orange', label="likely pathogenic", zorder=100)
+    frequent_likely_benign_freqs = [freqs for freqs in likely_benign_freqs.values() if freqs[0] > 0.25 or freqs[1] > 0.25]
+    frequent_ambigous_freqs = [freqs for freqs in ambigous_freqs.values() if freqs[0] > 0.25 or freqs[1] > 0.25]
+    frequent_likely_pathogenic_freqs = [freqs for freqs in likely_pathogenic_freqs.values() if freqs[0] > 0.25 or freqs[1] > 0.25]
+    ax.scatter(*np.array(frequent_likely_benign_freqs).T, marker='.', color='tab:gray', label="likely benign", zorder=1)
+    ax.scatter(*np.array(frequent_ambigous_freqs).T, marker='.', color='tab:orange', label="ambigous", zorder=10)
+    ax.scatter(*np.array(frequent_likely_pathogenic_freqs).T, marker='D', s=10, color='tab:orange', label="likely pathogenic", zorder=100)
 
     ax.legend()
     lims = (-0.02, 1.02)
@@ -89,6 +92,33 @@ def analyze_jpn_vs_global():
     ax.set_ylabel("Allele Freq. in 1KGP")
     fig.tight_layout()
     fig.savefig("./figures/4a_jpn_vs_global.pdf")
+    plt.close(fig)
+
+    # Fig. S4a
+    fig, ax = plt.subplots(1, 1, figsize=(4, 4), dpi=300)
+    ax.set_facecolor(color='#d4d4d4')
+
+    # Plot only AF <= 0.25
+    infrequent_likely_benign_freqs = [freqs for freqs in likely_benign_freqs.values() if freqs[0] <= 0.25 and freqs[1] <= 0.25]
+    infrequent_ambigous_freqs = [freqs for freqs in ambigous_freqs.values() if freqs[0] <= 0.25 and freqs[1] <= 0.25]
+    infrequent_likely_pathogenic_freqs = [freqs for freqs in likely_pathogenic_freqs.values() if freqs[0] <= 0.25 and freqs[1] <= 0.25]
+    ax.scatter(*np.array(infrequent_likely_benign_freqs).T, marker='.', color='tab:gray', label="likely benign", zorder=1)
+    ax.scatter(*np.array(infrequent_ambigous_freqs).T, marker='.', color='tab:orange', label="ambigous", zorder=10)
+    ax.scatter(*np.array(infrequent_likely_pathogenic_freqs).T, marker='D', s=10, color='tab:orange', label="likely pathogenic", zorder=100)
+
+    lims = (-0.01, 0.26)
+    ticks = [0, 0.05, 0.1, 0.15, 0.2, 0.25]
+    ax.set_xlim(*lims)
+    ax.set_xticks(ticks)
+    ax.set_xticklabels(ticks)
+    ax.set_yticks(ticks)
+    ax.set_yticklabels(ticks)
+    ax.set_ylim(*lims)
+    ax.set_aspect('equal')
+    ax.set_xlabel("Allele Freq. in 54KJPN")
+    ax.set_ylabel("Allele Freq. in 1KGP")
+    fig.tight_layout()
+    fig.savefig("./figures/S4a_jpn_vs_global.pdf")
     plt.close(fig)
 """
     # For Fig. 4b
